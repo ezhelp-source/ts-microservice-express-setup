d19ea2771419182a2e6ec6ea1f04a4a9cd53989a
João Carvalho <joaokvalho@gmail.com>
2020-10-08 10:57:49 -0300

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import { App } from './app'
 import { environment } from '@core/config/environment'
 
+import { productsRoutes } from '@api/products.routes'
+
 const PORT = environment.app.port
 
 async function bootstrap() {
   const app = new App()
 
-  const server = await app.create()
+  const server = await app.create([
+    productsRoutes
+  ])
 
   server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
